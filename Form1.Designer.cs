3420e72c6a02d0baa5b2bdc9a92c325036d5b239
mccreery-john-student <70606724+mccreery-john-student@users.noreply.github.com>
2025-02-07 11:50:48 -0500

    UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.space8 = new System.Windows.Forms.Button();
+            this.space7 = new System.Windows.Forms.Button();
+            this.space6 = new System.Windows.Forms.Button();
+            this.space1 = new System.Windows.Forms.Button();
+            this.space2 = new System.Windows.Forms.Button();
+            this.space5 = new System.Windows.Forms.Button();
+            this.space4 = new System.Windows.Forms.Button();
+            this.space3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.space0 = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.turnLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // space8
+            // 
+            this.space8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space8.Location = new System.Drawing.Point(645, 407);
+            this.space8.Name = "space8";
+            this.space8.Size = new System.Drawing.Size(82, 82);
+            this.space8.TabIndex = 43;
+            this.space8.Text = "X";
+            this.space8.UseVisualStyleBackColor = true;
+            // 
+            // space7
+            // 
+            this.space7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space7.Location = new System.Drawing.Point(541, 407);
+            this.space7.Name = "space7";
+            this.space7.Size = new System.Drawing.Size(82, 82);
+            this.space7.TabIndex = 42;
+            this.space7.Text = "X";
+            this.space7.UseVisualStyleBackColor = true;
+            // 
+            // space6
+            // 
+            this.space6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space6.Location = new System.Drawing.Point(437, 407);
+            this.space6.Name = "space6";
+            this.space6.Size = new System.Drawing.Size(82, 82);
+            this.space6.TabIndex = 41;
+            this.space6.Text = "X";
+            this.space6.UseVisualStyleBackColor = true;
+            // 
+            // space1
+            // 
+            this.space1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space1.Location = new System.Drawing.Point(541, 199);
+            this.space1.Name = "space1";
+            this.space1.Size = new System.Drawing.Size(82, 82);
+            this.space1.TabIndex = 40;
+            this.space1.Text = "X";
+            this.space1.UseVisualStyleBackColor = true;
+            // 
+            // space2
+            // 
+            this.space2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space2.Location = new System.Drawing.Point(645, 199);
+            this.space2.Name = "space2";
+            this.space2.Size = new System.Drawing.Size(82, 82);
+            this.space2.TabIndex = 39;
+            this.space2.Text = "X";
+            this.space2.UseVisualStyleBackColor = true;
+            // 
+            // space5
+            // 
+            this.space5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space5.Location = new System.Drawing.Point(645, 303);
+            this.space5.Name = "space5";
+            this.space5.Size = new System.Drawing.Size(82, 82);
+            this.space5.TabIndex = 38;
+            this.space5.Text = "X";
+            this.space5.UseVisualStyleBackColor = true;
+            // 
+            // space4
+            // 
+            this.space4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space4.Location = new System.Drawing.Point(541, 303);
+            this.space4.Name = "space4";
+            this.space4.Size = new System.Drawing.Size(82, 82);
+            this.space4.TabIndex = 37;
+            this.space4.Text = "X";
+            this.space4.UseVisualStyleBackColor = true;
+            // 
+            // space3
+            // 
+            this.space3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space3.Location = new System.Drawing.Point(437, 303);
+            this.space3.Name = "space3";
+            this.space3.Size = new System.Drawing.Size(82, 82);
+            this.space3.TabIndex = 36;
+            this.space3.Text = "X";
+            this.space3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(428, 391);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 10);
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox5.BackColor = System.Drawing.Color.Black;
+            this.pictureBox5.Location = new System.Drawing.Point(629, 190);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 308);
+            this.pictureBox5.TabIndex = 34;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(525, 190);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 308);
+            this.pictureBox4.TabIndex = 33;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(428, 287);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(308, 10);
+            this.pictureBox2.TabIndex = 32;
+            this.pictureBox2.TabStop = false;
+            // 
+            // space0
+            // 
+            this.space0.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.space0.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space0.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.space0.Location = new System.Drawing.Point(437, 199);
+            this.space0.Name = "space0";
+            this.space0.Size = new System.Drawing.Size(82, 82);
+            this.space0.TabIndex = 31;
+            this.space0.Text = "X";
+            this.space0.UseVisualStyleBackColor = true;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(541, 119);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(82, 52);
+            this.resetButton.TabIndex = 44;
+            this.resetButton.Text = "Reset Game";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // turnLabel
+            // 
+            this.turnLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.turnLabel.AutoSize = true;
+            this.turnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turnLabel.Location = new System.Drawing.Point(549, 509);
+            this.turnLabel.Name = "turnLabel";
+            this.turnLabel.Size = new System.Drawing.Size(60, 20);
+            this.turnLabel.TabIndex = 45;
+            this.turnLabel.Text = "Turn: X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1164, 648);
+            this.Controls.Add(this.turnLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.space8);
+            this.Controls.Add(this.space7);
+            this.Controls.Add(this.space6);
+            this.Controls.Add(this.space1);
+            this.Controls.Add(this.space2);
+            this.Controls.Add(this.space5);
+            this.Controls.Add(this.space4);
+            this.Controls.Add(this.space3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.space0);
+            this.MinimumSize = new System.Drawing.Size(369, 460);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button space8;
+        private System.Windows.Forms.Button space7;
+        private System.Windows.Forms.Button space6;
+        private System.Windows.Forms.Button space1;
+        private System.Windows.Forms.Button space2;
+        private System.Windows.Forms.Button space5;
+        private System.Windows.Forms.Button space4;
+        private System.Windows.Forms.Button space3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button space0;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label turnLabel;
     }
 }
 
